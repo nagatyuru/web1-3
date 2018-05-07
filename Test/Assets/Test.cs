f9a799ce72be2eca23620df0e80fcdc27b1a34d3
Unknown <evening68si2@gmail.com>
2018-05-07 17:32:45 +0900

    最初のコミット

--- Test/Assets/Test.cs
+++ Test/Assets/Test.cs
@@ -2,21 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Test : MonoBehaviour {
 
-	void Start () {
-        int sum = 0;
-        for(int i =1; i <= 10; i++)
-        {
-            sum += i;
-        }
-        
-            Debug.Log(sum);
-        
-    
-        
-       
+   
 
-       
-	}
+    void Start()
+    {
+        Vector2 startPos = new Vector2(2.0f, 1.0f);
+        Vector2 endPos = new Vector2(8.0f, 5.0f);
+        Vector2 dir = endPos - startPos;
+        Debug.Log(dir);
+
+        float len = dir.magnitude;
+        Debug.Log(len);
+
+    }
 }
